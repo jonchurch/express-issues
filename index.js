7a57fb7e84a40955ef979cb374b8c28e40940c48
Jon Church <me@jonchurch.com>
2020-05-21 16:43:06 -0400

    add in a wait async function

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const iThrow = require('./anotherModule');
 const app = express();
 
 app.get('/test', async (req, res, next) => {
+  await wait(3);
   await iThrow();
   res.json({ ok: true });
 });
 
-app.get('/', async (req, res, next) => {
-  throw new Error('Where did I occur?');
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, sec * 1000);
+  });
+}
+app.get('/', (req, res, next) =>
+  wait(3).then(() => Promise.reject(new Error('Where did I occur?')))
+);
+
+async function badMiddleware(req, res, next) {
+  await iThrow();
+}
+app.get('/middleware', badMiddleware, (req, res) => {
+  //
 });
 
 const PORT = process.env.PORT || 8000;
