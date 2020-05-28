a7649f336bf6b62173ef6b119ddb24e3f2849805
Jon Church <me@jonchurch.com>
2020-05-28 15:26:41 -0400

    body-parser issue 399

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/test', (req, res) => {
-  res.json({ ok: true });
+const PORT = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', function(req, res, next) {
+  console.log(`parsed Body: ${JSON.stringify(req.body)}`);
+  console.log(req.body.ring);
+  res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () =>
-  console.log(`Server listening on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`App now listening at http://localhost:${PORT}`);
+});
